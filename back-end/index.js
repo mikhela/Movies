36ebd34615b956ab7b5e1852298fda55bb4c91d4
Rohit Dorage <rohitdorage3@gmail.com>
2024-09-10 18:56:14 +0530

    Create a database connection

--- back-end/index.js
+++ back-end/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const { userRouter } = require("./routes/user");
+const dotenv = require("dotenv");
+dotenv.config();
 
 const app = express();
 
@@ -8,6 +11,24 @@ app.use("/user", userRouter);
 const port = 3000;
 const host = "localhost";
 
-app.listen(port, host, () => {
+const setupDb = async () => {
+  const client = new MongoClient(process.env.MONGO_URI, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  });
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+};
+
+app.listen(port, host, async () => {
+  await setupDb();
   console.log(`Example app listening at http://${host}:${port}`);
 });
